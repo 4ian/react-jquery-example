5a014edceedc5ad9e665176347b606d8246ccbae
Florian Rival <Florian.Rival@gmail.com>
2016-10-08 18:16:16 +0200

    Add jQuery and slide up a text with it

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import $ from "jquery";
 
 class App extends Component {
+  componentDidMount() {
+    // Here is a quick example if you (really want to) use jQuery
+    // with React :)
+    setTimeout(() => {
+      $(this.refs.intro).slideUp();
+    }, 200);
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,7 +19,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
+        <p className="App-intro" ref="intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
